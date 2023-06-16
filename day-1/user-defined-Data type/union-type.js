43acf63f067ca8fb1c45fb5b06c61a1477113314
Merajul Islam <meraj.exe@gmail.com>
2023-06-17 00:14:16 +0600

    video 5

---
+++ day-1/user-defined-Data type/union-type.js
@@ -0,0 +1,8 @@
+var user;
+user = 123;
+user = 'hello';
+function showNumbers(num1, num2) {
+    console.log(num1, num2);
+}
+showNumbers(1, 17);
+showNumbers('12', 14);
---
+++ day-1/user-defined-Data type/union-type.ts
@@ -0,0 +1,10 @@
+let user: number | string;
+user = 123;
+user = 'hello';
+
+function showNumbers(num1: number | string, num2: number | string) {
+  console.log(num1, num2);
+}
+
+showNumbers(1, 17);
+showNumbers('12', 14);
